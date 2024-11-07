d7cd0aa56fa7a90ad77aebcaff7ff3e8ec90f803
Ankush <ankushkdev55@gmail.com>
2024-11-07 20:11:08 +0530

    Update omniauth file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,'1013540422845-b7d791v6udjubdl4lilgkq4jdqpp6qci.apps.googleusercontent.com','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', {
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile', 
     prompt: 'consent',
     access_type: 'offline',
