ba825bfa3d2537c7494e5588e877dbc9ad869a79
Ankush <ankushkdev55@gmail.com>
2024-11-07 20:00:12 +0530

    update config file

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,11 @@ module ChatAppBackend
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
