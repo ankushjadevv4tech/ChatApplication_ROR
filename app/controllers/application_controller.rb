048d43649620dd92ee09ebbb0135678764a4c38b
Ankush <ankushkdev55@gmail.com>
2024-11-07 19:58:13 +0530

    Add current_user logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @current_user ||= User.find(decoded_token[:user_id]) if decoded_token
+  end
+
+  # Token decoding method
+  def decoded_token
+    if request.headers['Authorization'].present?
+      token = request.headers['Authorization'].split(' ').last
+      begin
+        JwtService.decode(token)
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  # Authentication check before any action
+  def authenticate_user!
+    render json: { error: 'Not Authorized' }, status: :unauthorized unless current_user
+  end
 end
